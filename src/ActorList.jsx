6e4417ab0d6b7c2d02e4b8706c0b5e1a749b308a
Aroa Cotrufo <aroa.c97@gmail.com>
2026-03-17 16:15:04 +0100

    milestone 2

--- src/ActorList.jsx
+++ src/ActorList.jsx
@@ -17,7 +17,26 @@ function ActorList() {
     }, [])
 
     return (
-        <h1>Lista Attori</h1>
+        <div className='mb-5'>
+            <h1 className='text-center display-4 mb-5'>Lista Attori</h1>
+            <div className='row g-4'>
+                {actors.map(actors => (
+                    <div key={actors.id} className='col-lg-4 col-md-6'>
+                        <div className=' card h-100 shadow-sm'>
+                            <h5> {actor.name}</h5>
+                            <p>Nazionalità: {actor.nationality}</p>
+                            <p>Anno: {actor.birth_years}</p>
+                            <p>{actor.bio}</p>
+                            <p>Riconoscimenti: {actor.awards}</p>
+                            <img src={actor.image} />
+                        </div>
+                    </div>
+                ))
+
+                }
+
+            </div>
+        </div>
     )
 }
 
--- src/ActressList.jsx
+++ src/ActressList.jsx
@@ -18,7 +18,26 @@ function ActressList() {
     }, [])
 
     return (
-        <h1>Lista Attrici</h1>
+    <div className='mb-5'>
+            <h1 className='text-center display-4 mb-5'>Lista Attori</h1>
+            <div className='row g-4'>
+                {actors.map(actress => (
+                    <div key={actress.id} className='col-lg-4 col-md-6'>
+                        <div className=' card h-100 shadow-sm'>
+                            <h5> {actress.name}</h5>
+                            <p>Nazionalità: {actress.nationality}</p>
+                            <p>Anno: {actress.birth_years}</p>
+                            <p>{actress.bio}</p>
+                            <p>Riconoscimenti: {actress.awards}</p>
+                            <img src={actress.image} />
+                        </div>
+                    </div>
+                ))
+
+                }
+
+            </div>
+        </div>
     )
 }
 export default ActressList
